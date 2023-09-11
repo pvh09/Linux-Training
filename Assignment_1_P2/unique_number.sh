aa31917a8d93aad615d79aea4404827a5413a315
pvh09 <93241474+pvh09@users.noreply.github.com>
2023-09-11 15:19:34 +0000

    update Ass 1 P2

--- Assignment_1_P2/unique_number.sh
+++ Assignment_1_P2/unique_number.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
-while true
-do
+
+while true; do
     read -p "Enter the number of integers (must be odd): " N
     if [[ $N =~ ^[1-9][0-9]*$ && $((N % 2)) -eq 1 ]]; then
         break
@@ -9,11 +9,19 @@ do
     fi
 done
 
-read -p "Enter $N space-separated integers: " A
+read -p "Enter $N space-separated integers: " numbers
 
-A=($A)
+# Use tr to replace spaces with newlines
+num_array=$(echo "$numbers" | tr ' ' '\n')
 
-# Loop through the array and process each integer
-for ((i = 0; i < N; i++)); do
-    echo "Integer $i: ${A[i]}"
-done
+# Use sort and uniq to find unique numbers
+unique_numbers=$(echo "$num_array" | sort | uniq -u)
+
+# Use awk to format the result with spaces
+unique_numbers=$(echo "$unique_numbers" | awk '{printf("%s ", $0)}')
+
+# Trim trailing whitespace
+unique_numbers="${unique_numbers%"${unique_numbers##*[![:space:]]}"}"
+
+echo "$unique_numbers"
+
