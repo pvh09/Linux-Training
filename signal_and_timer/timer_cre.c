e6fa171f85868e4d35837a6f6f1fd618bf1eefcf
pvh09 <huyhoa09012001@gmail.com>
2023-09-28 17:22:55 +0700

    update timer example

--- signal_and_timer/timer_cre.c
+++ signal_and_timer/timer_cre.c
@@ -27,38 +27,46 @@ int main()
     sev.sigev_value.sival_ptr = &timerid;
     timer_create(CLOCK_REALTIME, &sev, &timerid);
 
-    while (retry_count < 3) {
+    while (retry_count < 3)
+    {
         its_terminate.it_value.tv_sec = 5;
         its_terminate.it_value.tv_nsec = 0;
         its_terminate.it_interval.tv_sec = 3;
         its_terminate.it_interval.tv_nsec = 0;
         timer_settime(timerid, 0, &its_terminate, NULL);
 
-        while (1) {
+        while (1)
+        {
             // Get the current timer value
             timer_gettime(timerid, &its_terminate);
             time_t remaining_seconds = its_terminate.it_value.tv_sec;
 
-            if (remaining_seconds > 0){
+            if (remaining_seconds > 0)
+            {
                 printf("Remaining time before expiration: %ld seconds\n", remaining_seconds);
             }
-            else{
+            else
+            {
                 printf("Timer has expired!\n");
                 break;
             }
             sleep(1);
         }
         retry_count++;
-        if (retry_count < 3) {
+        if (retry_count < 3)
+        {
             printf("Retrying...\n");
             sleep(1); // Wait before retrying
         }
     }
 
-    if (timer_delete(timerid) == -1){
+    if (timer_delete(timerid) == -1)
+    {
         perror("timer_delete");
         exit(EXIT_FAILURE);
-    } else{
+    }
+    else
+    {
         printf("Timer %ld deleted successfully.\n", (long)timerid);
     }
 
